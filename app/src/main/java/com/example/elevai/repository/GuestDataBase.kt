a50490d399cdb7adc2af4a69a64576a545d9be1c
Thales Ruan <thalesruandamassacla@gmail.com>
2023-02-06 19:39:48 -0300

    criação sqLite table

---
+++ app/src/main/java/com/example/elevai/repository/GuestDataBase.kt
@@ -0,0 +1,29 @@
+package com.example.elevai.repository
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class GuestDataBase(
+    context: Context
+) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+
+    companion object{
+        private const val NAME = "guesdb"
+        private const val VERSION = 1
+    }
+
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("CREATE TABLE Guest" +
+                " (id integer primary key autoincrement," +
+                " name text," +
+                " presence integer);")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+
+    }
+
+
+}
