132896f7418eb3dfefc990b3ce1184664022745f
Thales Ruan <thalesruandamassacla@gmail.com>
2023-02-06 19:01:12 -0300

    criação repository

--- app/src/main/java/com/example/elevai/GuestFormViewModel.kt
+++ app/src/main/java/com/example/elevai/GuestFormViewModel.kt
@@ -4,6 +4,6 @@ import androidx.lifecycle.ViewModel
 
 class GuestFormViewModel : ViewModel() {
 
-    private val repository = GuestRepository()
+    private val repository = GuestRepository.getInstance()
 
 }
--- app/src/main/java/com/example/elevai/GuestRepository.kt
+++ app/src/main/java/com/example/elevai/GuestRepository.kt
@@ -2,4 +2,15 @@ package com.example.elevai
 
 class GuestRepository {
 
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(): GuestRepository {
+            if (!::repository.isInitialized) {
+                repository = GuestRepository()
+            }
+            return repository
+        }
+    }
+
 }
