a134e547df2eaac796c684c49c2fe294165481e3
Thales Ruan <thalesruandamassacla@gmail.com>
2023-02-07 18:35:06 -0300

    update

--- app/src/main/java/com/example/elevai/repository/GuestDataBase.kt
+++ app/src/main/java/com/example/elevai/repository/GuestDataBase.kt
@@ -3,22 +3,24 @@ package com.example.elevai.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.elevai.contantes.DataBaseConstantes
 
 class GuestDataBase(
     context: Context
 ) : SQLiteOpenHelper(context, NAME, null, VERSION) {
 
-    companion object{
+    companion object {
         private const val NAME = "guesdb"
         private const val VERSION = 1
     }
 
-
+//todo se der erro pode ter sido nessa concatenação
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL("CREATE TABLE Guest" +
-                " (id integer primary key autoincrement," +
-                " name text," +
-                " presence integer);")
+        db.execSQL("CREATE TABLE " + DataBaseConstantes.GUEST.TABLE_NAME +" ( "+
+                     DataBaseConstantes.GUEST.COLUMNS.ID + " integer primary key autoincrement," +
+                    DataBaseConstantes.GUEST.COLUMNS.NAME + "  text," +
+                    DataBaseConstantes.GUEST.COLUMNS.PRESENCE + " integer);"
+        )
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
--- app/src/main/java/com/example/elevai/repository/GuestRepository.kt
+++ app/src/main/java/com/example/elevai/repository/GuestRepository.kt
@@ -2,6 +2,7 @@ package com.example.elevai.repository
 
 import android.content.ContentValues
 import android.content.Context
+import com.example.elevai.contantes.DataBaseConstantes
 import com.example.elevai.model.GuestModel
 
 class GuestRepository private constructor(context: Context) {
@@ -25,17 +26,37 @@ class GuestRepository private constructor(context: Context) {
             val presence = if (guest.presence) 1 else 0
             val values = ContentValues()
 
-            values.put("name", guest.name)
-            values.put("presence", presence)
+            values.put(DataBaseConstantes.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstantes.GUEST.COLUMNS.PRESENCE, presence)
 
-            db.insert("Guest", null, values)
+            db.insert(DataBaseConstantes.GUEST.TABLE_NAME, null, values)
             true
         } catch (e: java.lang.Exception) {
             false
         }
     }
 
-    fun update() {
+    fun update(guest: GuestModel) : Boolean{
+
+        return  try {
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+            val values = ContentValues()
+
+            values.put(DataBaseConstantes.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstantes.GUEST.COLUMNS.PRESENCE, presence)
+
+
+            val selection = DataBaseConstantes.GUEST.COLUMNS.ID + "id = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstantes.GUEST.TABLE_NAME, values, selection,args)
+            true
+        }catch (e:java.lang.Exception){
+            false
+        }
+
+
 
     }
 }
